29557dcbcaaaf6b2f949d1d36b9f7480f5155a5b
involucelate <involucelate@gmail.com>
2014-10-16 22:38:28 -0400

    ch 5 partial

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "StaticPages" do
   describe "Home page" do
 
     it 'should have the content "Sample App"' do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it 'should have the right title' do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title "#{title_prefix}"
     end
 
     it 'should have the right title' do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title "| Home"
     end
 
@@ -26,12 +26,12 @@ describe "StaticPages" do
   describe "help page" do
 
     it 'should have the content "Help"' do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it 'should have the right title' do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title "#{title_prefix} | Help"
     end
 
@@ -40,12 +40,12 @@ describe "StaticPages" do
   describe "about page" do
 
     it 'should have the content "About"' do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
 
     it 'should have the right title' do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title "#{title_prefix} | About"
     end
 
@@ -54,12 +54,12 @@ describe "StaticPages" do
   describe "contact page" do
 
     it 'should have the content "Contact"' do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it 'should have the right title' do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title "#{title_prefix} | Contact"
     end
 
